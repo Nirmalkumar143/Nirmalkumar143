89698299e1400f58e5a946836f0fd339a9983791
Nirmalkumar143 <premkumarpp7894886@gmail.com>
2024-11-30 21:41:17 +0530

    Create README.c
    to find even numbers

---
+++ README.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+  int num;
+  printf("enter the number ");
+  scanf("%d", &num):
+    if(num%2==0){
+      printf("it is even number"):
+
+        }
+        else{
+     printf("it is negative number");
+        }
+return 0;
+
+}
+
+<!---
+Nirmalkumar143/Nirmalkumar143 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
